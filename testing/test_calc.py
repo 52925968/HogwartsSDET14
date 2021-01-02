2e92d5ac1c62703dce7e2ee9b3b07b8c35e6d9cb
5968 <13552925968@163.com>
2021-01-02 19:03:24 +0800

    第3次提交

--- testing/test_calc.py
+++ testing/test_calc.py
@@ -2,6 +2,8 @@
 import pytest
 import sys
 
+import yaml
+
 print(sys.path.append('..'))
 from pythoncode.calc import Calculator
 
@@ -14,20 +16,24 @@ class TestCalc:
     def teardown_class(self):
         print("类级别2222")
 
+    # 增加标签（可以单独运行某个标签）
     @pytest.mark.add
     def test_add(self):
         # cal=Calculator()
         assert 2 == self.cal.add(1, 1)
 
-    @pytest.mark.add
-    def test_add1(self):
+    # 使用yamL的参数化
+    @pytest.mark.parametrize(("sum", "a", "b"), yaml.safe_load(open("./data.yaml")))
+    def test_add1(self, sum, a, b):
         # cal=Calculator()
-        assert 2 == self.cal.add(1, 2)
+        assert sum == self.cal.add(a, b)
 
     def test_add3(self):
         # cal = Calculator()
         assert 2 == self.cal.add(1, 3)
 
-    def test_div(self):
+    # 参数化
+    @pytest.mark.parametrize("sum,a,b", [(3, 2, 2), (5, 5, 1)])
+    def test_div(self, sum, a, b):
         # cal=Calculator()
-        assert 2 == self.cal.div(2, 1)
+        assert sum == self.cal.div(a, b)
